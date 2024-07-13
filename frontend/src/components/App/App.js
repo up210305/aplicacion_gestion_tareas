eec99958dd9a3454e0f71454eafee3a8ba55c267
up210305 <112642874+up210305@users.noreply.github.com>
2024-07-13 00:37:20 -0600

    Changes in App

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import './Task/AddTask.js';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Bienvenido a la Aplicación de Gestión de Tareas</h1>
+        {/* Aquí puedes agregar otros componentes y lógica de tu aplicación */}
       </header>
     </div>
   );
