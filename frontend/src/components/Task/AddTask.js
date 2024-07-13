eec8fd76de4b95c82f475b77107b962bd63ba99a
up210305 <112642874+up210305@users.noreply.github.com>
2024-07-13 00:15:38 -0600

    Changes in App

--- frontend/src/components/Task/AddTask.js
+++ frontend/src/components/Task/AddTask.js
@@ -1,11 +1,18 @@
+// src/components/AddTask.js
 import React from 'react';
+import './AddTask.css'; // Puedes crear un archivo de estilos específico para cada componente
 
 const AddTask = () => {
   return (
-    <div>
-      <h1>AddTask Component</h1>
+    <div className="add-task">
+      <input type="text" placeholder="Add a new task..." />
+      <div className="task-icons">
+        <i className="fas fa-plus"></i>
+        <i className="fas fa-calendar-alt"></i>
+        <i className="fas fa-bell"></i>
+      </div>
     </div>
   );
-};
+}
 
 export default AddTask;
