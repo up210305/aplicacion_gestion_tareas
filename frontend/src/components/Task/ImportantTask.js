3a68f3fa7d92977fc7d41a9b37db506025ec98c4
up210305 <112642874+up210305@users.noreply.github.com>
2024-07-14 19:27:33 -0600

    Creation of ImportantTask Component

---
+++ frontend/src/components/Task/ImportantTask.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+const ImportantTask = () => {
+  return (
+    <div>
+      <Checkbox
+        {...label}
+        icon={<BookmarkBorderIcon />}
+        checkedIcon={<BookmarkIcon />}
+      />
+    </div>
+  );
+}
+
+export default ImportantTask;
