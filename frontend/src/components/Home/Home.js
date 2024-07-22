889575ea1293581d2f23ac884dd3da9200df101c
Alan <up210923@alumnos.upa.edu.mx>
2024-07-22 03:42:52 -0600

    Prueba Home

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,12 +1,70 @@
 import React from 'react';
-import './Home.css';
+import { Card, CardContent, Grid, Typography, Box } from '@mui/material';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 
-const Home = () => {
+const TaskManager = () => {
   return (
-    <div>
-      <h1>Home Component</h1>
-    </div>
-  );
-};
+    <Box sx={{ padding: 4 }}>
+      <Grid container spacing={4}>
+        {/* Recent Tasks */}
+        <Grid item xs={12}>
+          <Box display="flex" alignItems="center" mb={2}>
+            <AccessTimeIcon fontSize="large" />
+            <Typography variant="h5" ml={1}>Recent Tasks</Typography>
+          </Box>
+          <Grid container spacing={2}>
+            {[1, 2, 3].map((_, index) => (
+              <Grid item xs={12} md={4} key={index}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="body2" color="textSecondary">Task {index + 1}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
 
-export default Home;
+        {/* Today's Tasks */}
+        <Grid item xs={12}>
+          <Box display="flex" alignItems="center" mb={2}>
+            <WbSunnyIcon fontSize="large" />
+            <Typography variant="h5" ml={1}>Today</Typography>
+          </Box>
+          <Grid container spacing={2}>
+            {[1, 2, 3].map((_, index) => (
+              <Grid item xs={12} md={4} key={index}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="body2" color="textSecondary">Task {index + 1}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+
+        {/* Upcoming Tasks */}
+        <Grid item xs={12}>
+          <Box display="flex" alignItems="center" mb={2}>
+            <CalendarTodayIcon fontSize="large" />
+            <Typography variant="h5" ml={1}>Upcoming Tasks</Typography>
+          </Box>
+          <Grid container spacing={2}>
+            {[1, 2, 3].map((_, index) => (
+              <Grid item xs={12} md={4} key={index}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="body2" color="textSecondary">Task {index + 1}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
