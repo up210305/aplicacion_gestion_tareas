66f5fa0edee43a40a3285b93928c6d118cffd5be
UP210342 <up210342@alumnos.upa.edu.mx>
2024-08-01 00:29:46 -0600

    Correcion DB

--- backend/src/main/java/com/aplicacion_gestion_tareas/aplicacion_gestion_tareas/repository/TaskListRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aplicacion_gestion_tareas.aplicacion_gestion_tareas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.aplicacion_gestion_tareas.aplicacion_gestion_tareas.model.TaskList;
-
-public interface TaskListRepository extends JpaRepository<TaskList, Long> {
-}
