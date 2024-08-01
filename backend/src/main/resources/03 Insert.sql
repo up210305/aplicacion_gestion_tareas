d67e809be7e7fc36b6c62880be89edcbdf2bf15f
up210305 <112642874+up210305@users.noreply.github.com>
2024-08-01 14:27:57 -0600

    Update of the Database

--- backend/src/main/resources/03 Insert.sql
+++ backend/src/main/resources/03 Insert.sql
@@ -1,12 +1,17 @@
 use GESTION_TAREAS;
 
+-- Insertar datos en la tabla employees
 INSERT INTO employees (last_name, first_name, username, password, active) VALUES
 ('Adminson', 'Admin', 'admin', 'admin', true);
 
-INSERT INTO lists (list_name, description) VALUES 
-('Lista de Tareas Diarias', 'Lista para gestionar las tareas diarias del equipo de trabajo'),
-('Lista de Proyectos', 'Lista para administrar los diferentes proyectos en curso'),
-('Lista de Compras', 'Lista de artículos que necesitan ser comprados para el almacén');
+-- Insertar datos en la tabla lists
+INSERT INTO lists (list_name, description, id_employee) VALUES 
+('Lista de Tareas Diarias', 'Lista para gestionar las tareas diarias del equipo de trabajo', 1),
+('Lista de Proyectos', 'Lista para administrar los diferentes proyectos en curso', 1),
+('Lista de Compras', 'Lista de artículos que necesitan ser comprados para el almacén', 1);
 
+-- Insertar datos en la tabla tasks
 INSERT INTO tasks (task_title, task_description, creation_date, expire_date, completed, important, id_employee, id_list) VALUES
-('TEST', 'TESTING', '2024-7-31', '2024-8-30', false, false, '1', '1');
+('TEST', 'TESTING', '2024-07-31', '2024-08-30', false, false, 1, 1),
+('TEST2', 'TESTING', '2024-07-31', '2024-08-30', false, false, 1, 2),
+('TEST3', 'TESTING', '2024-07-31', '2024-08-30', false, false, 1, 3);
