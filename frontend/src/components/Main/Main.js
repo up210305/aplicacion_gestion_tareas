70ffc1b240cb1c9bf0fb8544e772db1f0bca724b
UP210342 <up210342@alumnos.upa.edu.mx>
2024-08-01 18:26:56 -0600

    Home, Tasks and Lists

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import { Brightness4, WbSunny } from '@mui/icons-material';
 import Box from '@mui/material/Box';
-=======
-// src/components/Main/Main.js
-import React, { useState, useMemo, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
->>>>>>> 28be44ee16813cb362a7cd033cc11388ba05a3a4
 import CssBaseline from '@mui/material/CssBaseline';
 import IconButton from '@mui/material/IconButton';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -14,21 +7,12 @@ import React, { useEffect, useMemo, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Aside from '../Aside'; // Import the Aside component
 import Home from '../Home';
+import ImportantTasks from '../ImportantTasks/ImportantTasks'; // Ruta corregida
 import List from '../List'; // Import List component
 import NewList from '../NewList';
 import QList from '../QList';
 import SignIn from '../SignIn';
 import SignUp from '../SignUp';
-<<<<<<< HEAD
-=======
-import QList from '../QList';
-import NewList from '../NewList';
-import Home from '../Home';
-import ImportantTasks from '../ImportantTasks/ImportantTasks'; // Ruta corregida
-import List from '../List';  
-import { Brightness4, WbSunny } from '@mui/icons-material';
-import Aside from '../Aside'; 
->>>>>>> 28be44ee16813cb362a7cd033cc11388ba05a3a4
 
 function Main() {
   const [darkMode, setDarkMode] = useState(() => {
@@ -68,12 +52,8 @@ function Main() {
             <Route path="/importanttasks" element={<ImportantTasks darkMode={darkMode} />} />
             <Route path="/new-list" element={<NewList darkMode={darkMode} />} />
             <Route path="/home" element={<Home />} />
-<<<<<<< HEAD
             <Route path="/list/:listId" element={<List darkMode={darkMode} />} /> {/* Ensure this route matches */}
             <Route path="/tasks/:listId" element={<List darkMode={darkMode} />} /> {/* Ensure this route matches */}
-=======
-            <Route path="/list/:listId" element={<List darkMode={darkMode} />} />
->>>>>>> 28be44ee16813cb362a7cd033cc11388ba05a3a4
           </Routes>
         </Box>
       </Box>
