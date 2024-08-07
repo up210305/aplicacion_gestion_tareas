7b2d630fbad39a22a8ff6f6b978b7c66c3665eb7
up210305 <112642874+up210305@users.noreply.github.com>
2024-08-07 01:57:57 -0600

    Solve error in Home

---
+++ frontend/src/components/services/taskService.js
@@ -0,0 +1,8 @@
+// src/services/taskService.js
+export const fetchTasks = async () => {
+    const response = await fetch('http://localhost:8080/api/tasks');
+    if (!response.ok) {
+      throw new Error('Error fetching tasks');
+    }
+    return response.json(); // Asegúrate de que la respuesta es un JSON válido
+  };
